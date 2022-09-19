aa3ba855b86203c012e39665860f634476740a38
IgorMarcheley <igormarcheley@gmail.com>
2022-09-19 20:33:14 -0300

    Adicionando escolher cores

--- script.js
+++ script.js
@@ -5,3 +5,9 @@ menu.addEventListener('click', () => {
     menu.classList.toggle('ativo');
     NavMenu.classList.toggle('ativo');
 })
+
+type="text/javascript">
+        /*Função de mudar o celular*/
+        function imgSlider(e){
+            document.querySelector('#phone').src = e;
+        }
